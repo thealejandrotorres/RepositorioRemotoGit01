8db45362a0361f59c22ad1703757d76a45568784
Diurno <Diurno@pc0107>
2023-05-05 13:38:10 +0200

    Tercera Confirmación

--- A5_ControlVersiones/src/controlDeVersiones/Principal.java
+++ A5_ControlVersiones/src/controlDeVersiones/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Control de Versiones con Git.");
 		System.out.println("Nueva Línea.");
+		System.out.println("Segunda nueva línea.");
 	}
 
 }
